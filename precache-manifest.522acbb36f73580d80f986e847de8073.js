2ad20330b32451882d842ca162adf829ea2023b8
rahul-ashu-kumar <1990rahulkumar@gmail.com>
2020-12-18 14:02:04 +0530

    Updates

---
+++ precache-manifest.522acbb36f73580d80f986e847de8073.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d8c13d77aade127549d27bf0fba8587",
+    "url": "/Rahul-ashu-Kumar/nextrproj/index.html"
+  },
+  {
+    "revision": "55d8700b3aee5c7c8c66",
+    "url": "/Rahul-ashu-Kumar/nextrproj/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "cc6ba16e47f479ab600f",
+    "url": "/Rahul-ashu-Kumar/nextrproj/static/css/main.17e1066c.chunk.css"
+  },
+  {
+    "revision": "55d8700b3aee5c7c8c66",
+    "url": "/Rahul-ashu-Kumar/nextrproj/static/js/2.82381c33.chunk.js"
+  },
+  {
+    "revision": "f7e390b48f899bb4fe63d2cbd1c93d28",
+    "url": "/Rahul-ashu-Kumar/nextrproj/static/js/2.82381c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6ba16e47f479ab600f",
+    "url": "/Rahul-ashu-Kumar/nextrproj/static/js/main.28f222f7.chunk.js"
+  },
+  {
+    "revision": "941560489452b0656c1c",
+    "url": "/Rahul-ashu-Kumar/nextrproj/static/js/runtime-main.da062b56.js"
+  },
+  {
+    "revision": "545dbe14c7ceba026b81fd9f5a21369e",
+    "url": "/Rahul-ashu-Kumar/nextrproj/static/media/logoNextra.545dbe14.png"
+  }
+]);
